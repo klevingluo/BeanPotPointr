c4da0ef49eaa03685cfae9b32a777aa311b430f2
npmw10533 <wottonn@gmail.com>
2015-02-08 03:27:13 -0500

    cadsf

--- mobile/src/main/java/com/oscode/pointr/YelpAPI.java
+++ mobile/src/main/java/com/oscode/pointr/YelpAPI.java
@@ -29,7 +29,7 @@ public class YelpAPI extends AsyncTask<Void, Void, Void> {
     private static final String API_HOST = "api.yelp.com";
     private static final String SEARCH_PATH = "/v2/search";
     private static final String BUSINESS_PATH = "/v2/business";
-    private static final int SEARCH_LIMIT = 10;
+    private static final int SEARCH_LIMIT = 5;
     private static final int SORT_TYPE = 1;
     private String term = "food";
     private String longitude;
@@ -168,7 +168,7 @@ public class YelpAPI extends AsyncTask<Void, Void, Void> {
             e.printStackTrace();
         }
         JSONProcessor.locations = new ArrayList<Locals>();
-        for (int i = 0; i < 10; i++) {//<---- change back to JSONProcessor.data.length() instead of 10 if you want more results
+        for (int i = 0; i < JSONProcessor.data.length(); i++) {
             try {
                 JSONObject temp = JSONProcessor.data.getJSONObject(i);
                 if (!temp.getBoolean("is_closed")) {
